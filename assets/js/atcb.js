ac5c7c57afe338efb9d6ef0cbea47cf84bc9dd51
AJRepo <AJRepo@users.noreply.github.com>
2022-01-21 22:08:42 -0600

    Add browserTz option

--- assets/js/atcb.js
+++ assets/js/atcb.js
@@ -530,21 +530,25 @@ function atcb_generate_time(data, style = 'delimiters', targetCal = 'general') {
         end = end.replace(/\-/g, '').replace(/\:/g, '');
       }
     } else {
-      // if there is no offset, we prepare the time, letting the browser set the local timzone
+      // Create time object (defaults to UTC)
       start = new Date( startDate[2] + '-' + startDate[0] + '-' + startDate[1] + 'T' + data['startTime'] + ':00.000+00:00' );
       end = new Date( endDate[2] + '-' + endDate[0] + '-' + endDate[1] + 'T' + data['endTime'] + ':00.000+00:00' );
-      if (data['timeZone'] != null && data['timeZone'] != '') {
+      if (data['timeZone'] != null && data['timeZone'] != '' && data['browserTz'] != true) {
         // if a timezone is given, we adjust dynamically with the modern toLocaleString function
         let utcDate = new Date(start.toLocaleString('en-US', { timeZone: "UTC" }));
         let tzDate = new Date(start.toLocaleString('en-US', { timeZone: data['timeZone'] }));
-        // let offset = utcDate.getTime() - tzDate.getTime();
-        start.setTime( start.getTime() );
-        end.setTime( end.getTime() );
+        let offset = utcDate.getTime() - tzDate.getTime();
+        start.setTime( start.getTime() + offset );
+        end.setTime( end.getTime() + offset );
+      } else {
+        // If we want to use the browser's timezone, remove the UTC timezone mark
+        if (data['browserTz'] == true) {
+          start = start.replace('Z', '');
+          end = end.replace('Z', '');
+        }
       }
       start = start.toISOString().replace('.000', '');
       end = end.toISOString().replace('.000', '');
-      start = start.replace('Z', '');
-      end = end.replace('Z', '');
       if (style == 'clean') {
         start = start.replace(/\-/g, '').replace(/\:/g, '');
         end = end.replace(/\-/g, '').replace(/\:/g, '');
