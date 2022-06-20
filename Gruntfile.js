15bfde798fce893fe6c72d47cb7b10ed0c81740c
Jens Kuerschner <jekuer@users.noreply.github.com>
2022-06-20 17:22:28 +0200

    grunt style fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-const initCodeDelimiter = /\/\/ START INIT[\s\S]*?\/\/ END INIT/gm
+const initCodeDelimiter = /\/\/ START INIT[\s\S]*?\/\/ END INIT/g
 
 function process(content, exportPhrase) {
   return content.replace(initCodeDelimiter, `${exportPhrase} { atcb_action, atcb_init };`);
@@ -7,7 +7,8 @@ module.exports = function(grunt) {
 
   // The config.
   grunt.initConfig({
-    version: { // update version. Either use via `npm run release --set=version::patch`, `npm run release --set=version::minor`, `npm run release --set=version::major`, or `npm run release --set=version::x.x.x` (with x.x.x being the exact version number)
+    // update version. Either use via `npm run release --set=version::patch`, `npm run release --set=version::minor`, `npm run release --set=version::major`, or `npm run release --set=version::x.x.x` (with x.x.x being the exact version number)
+    version: {
       package: {
         src: ['package.json']
       },
@@ -30,10 +31,12 @@ module.exports = function(grunt) {
         src: ['assets/css/atcb.css']
       },
     },
-    clean: { // cleans old built files
+    // cleans old built files
+    clean: {
       oldBuildFiles: ['assets/js/atcb.min.js', 'assets/js/atcb.min.js.map', 'assets/css/atcb.min.css', 'assets/css/atcb.min.css.map', 'npm_dist/']
     },
-    copy: { // creates the source files for the npm versionm supporting CommonJS and ES Module (https://www.sensedeep.com/blog/posts/2021/how-to-create-single-source-npm-module.html)
+    // creates the source files for the npm versionm supporting CommonJS and ES Module (https://www.sensedeep.com/blog/posts/2021/how-to-create-single-source-npm-module.html)
+    copy: {
       mjs_dist: {
         src: 'assets/js/atcb.js',
         dest: 'npm_dist/mjs/index.js',
@@ -45,7 +48,8 @@ module.exports = function(grunt) {
         options: { process: content => process(content, "module.exports =") }
       }
     },
-    uglify: { // minifies the main js file
+    // minifies the main js file
+    uglify: {
       options: {
         compress: true,
         mangle: true,
@@ -57,7 +61,8 @@ module.exports = function(grunt) {
         }
       }
     },
-    cssmin: { // minifies the main css file
+    // minifies the main css file
+    cssmin: {
       options: {
         sourceMap: true
       },
@@ -70,11 +75,11 @@ module.exports = function(grunt) {
   });
 
   // Load the plugins.
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-version');
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-version");
 
   // Register task(s).
   grunt.registerTask('default', ['clean', 'copy', 'uglify', 'cssmin']);
