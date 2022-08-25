76430511fcf53a2db41ea91e110d6a1b56dcb9a5
Jens Kuerschner <jekuer@users.noreply.github.com>
2022-08-25 08:35:55 +0200

    sub-branch update (#145)
    * update Gruntfile
    
    * workflow fixes
    
    * fixing tests
    
    * fixing test
    
    * test optimization

--- test/tests.js
+++ test/tests.js
@@ -10,6 +10,7 @@ try {
     console.log('TEST SUCCESSFUL: importing the script as module\n');
   } catch (error) {
     console.log('FAILED: Something went wrong with testing the ES Module setup\n');
+    throw error;
   }
 
   try {
@@ -17,6 +18,7 @@ try {
     console.log('TEST SUCCESSFUL: commonJS init via require\n');
   } catch (error) {
     console.log('FAILED: Something went wrong with testing the commonJS setup\n');
+    throw error;
   }
 
   try {
@@ -24,16 +26,19 @@ try {
     console.log('TEST SUCCESSFUL: server side rendering seems not to be blocked\n');
   } catch (error) {
     console.log('FAILED: Something went wrong with the server side rendering test\n');
+    throw error;
   }
 
   const fs = require('fs');
   const dirToDrop = 'npm_dist';
-  fs.rm(dirToDrop, { recursive: true }, (err) => {
-    if (err) {
-      throw err;
+  fs.rm(dirToDrop, { recursive: true }, (error) => {
+    if (error) {
+      throw error;
     }
-    console.log(`${dirToDrop} deleted again\n`);
+    console.log(`${dirToDrop} deleted again`);
+    console.log('All Tests SUCCESSFUL!\n');
   });
 } catch (error) {
-  console.log('FAILED: Something went wrong with the npm build script while testing');
+  console.log('FAILED: Something went wrong with the npm build script while testing\n');
+  throw error;
 }
