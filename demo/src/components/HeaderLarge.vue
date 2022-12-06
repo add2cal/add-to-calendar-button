617f189738c571cc9893a15572c4208d49177537
Jens Kuerschner <mail@jenskuerschner.de>
2022-12-06 22:19:36 +0100

    minor demo page updates

--- demo/src/components/HeaderLarge.vue
+++ demo/src/components/HeaderLarge.vue
@@ -2,18 +2,46 @@
 import NavigationBar from "@/components/NavigationBar.vue";
 import StatsBar from "@/components/StatsBar.vue";
 import Logo from "@/components/LogoMain.vue";
+import { CalendarDaysIcon } from '@heroicons/vue/24/outline';
+import { RouterLink } from "vue-router";
+import { useI18n } from 'vue-i18n'
+const { locale } = useI18n();
 </script>
 
 <template>
   <div
     class="w-full bg-gradient-to-tr from-primary via-primary to-primary-light py-8 shadow-lg dark:from-primary dark:via-primary-dark dark:to-primary-dark"
   >
-    <div class="container flex w-full flex-row-reverse"><NavigationBar /></div>
+    <div class="container flex justify-between">
+      <div class="-mt-1 w-auto max-w-[150px] flex-grow">
+        <RouterLink
+          v-if="locale=='en'"
+          :to="{ name: 'home', params: { locale } }"
+        >
+          <Logo class="force-light" />
+        </RouterLink>
+        <RouterLink
+          v-if="locale!='en'"
+          :to="{ name: 'home-i18n', params: { locale } }"
+        >
+          <Logo class="force-light" />
+        </RouterLink>
+      </div>
+      <NavigationBar />
+    </div>
     <div class="container m-auto my-16 flex items-center justify-between">
       <div
         class="mx-auto w-auto max-w-[400px] flex-grow pb-0 lg:mx-20 xl:mx-14 xl:pb-8 2xl:mx-0"
       >
-        <Logo class="force-light" />
+        <h1>
+          <span class="block">
+            <CalendarDaysIcon
+              class="-mt-1 mr-3 inline-block h-6 w-6"
+              aria-hidden="true"
+            />Welcome to your next
+          </span>
+          <span class="mb-6 block">Add to Calendar Button!</span>
+        </h1>
       </div>
       <div class=" mr-30 hidden self-end pb-3 lg:block xl:mr-60">
         <img
---
+++ demo/src/components/integration/IntegrationGuides.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+import { useI18n } from 'vue-i18n'
+const { t, locale } = useI18n();
+</script>
+
+<template>
+  <div>
+    <p>
+      The script is built as a "Web Component"!<br />
+      This means that you can use it similar to any other HTML element and it is
+      working out-of-the-box for all setups.
+    </p>
+    <p>
+      However, if you are using some kind of framework or library, you might
+      want to check out the specific guides, to get it even stronger
+      integrated.<br />
+      No worries! This makes it even more convenient.
+    </p>
+    <div class="m-20 flex justify-between">
+      <RouterLink class="border p-20" :to="{ name: 'use', params: { locale } }">
+        {{ t('navigation.advanced') }}
+      </RouterLink>
+      <RouterLink
+        class="border p-20"
+        :to="{ name: 'use-with-angular', params: { locale } }"
+      >
+        {{ t('navigation.advanced') }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
