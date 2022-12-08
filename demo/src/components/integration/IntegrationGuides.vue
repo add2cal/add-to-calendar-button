cbe11dee2ff8dd0bd6c70e8f8e7969607aa35f49
Jens Kuerschner <mail@jenskuerschner.de>
2022-12-08 09:55:50 +0100

    update

--- demo/src/components/integration/IntegrationGuides.vue
+++ demo/src/components/integration/IntegrationGuides.vue
@@ -26,42 +26,42 @@ const { t, locale } = useI18n();
         class="mx-auto grid grid-cols-1 gap-14 xs:grid-cols-2 md:grid-cols-3"
       >
         <RouterLink
-          class="group flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-angular hover:from-angular hover:to-angular hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-angular dark:hover:from-angular dark:hover:to-angular dark:hover:text-white"
+          class="tech-stack-button group h-36 w-40 hover:border-angular hover:from-angular hover:to-angular hover:text-white dark:hover:border-angular dark:hover:from-angular dark:hover:to-angular dark:hover:text-white"
           :to="{ name: 'use-with-angular', params: { locale } }"
         >
           <Angular class="self-center p-4" />
           <div class="self-center p-4 pt-0">Angular</div>
         </RouterLink>
         <RouterLink
-          class="group flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-astro hover:from-astro hover:to-astro hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-astro dark:hover:from-astro dark:hover:to-astro dark:hover:text-white"
+          class="tech-stack-button group h-36 w-40 hover:border-astro hover:from-astro hover:to-astro hover:text-white dark:hover:border-astro dark:hover:from-astro dark:hover:to-astro dark:hover:text-white"
           :to="{ name: 'use-with-astro', params: { locale } }"
         >
           <Astro class="self-center p-4" />
           <div class="self-center p-4 pt-0">Astro</div>
         </RouterLink>
         <RouterLink
-          class="group flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-react hover:from-react hover:to-react hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-react dark:hover:from-react dark:hover:to-react dark:hover:text-white"
+          class="tech-stack-button group h-36 w-40 hover:border-react hover:from-react hover:to-react hover:text-white dark:hover:border-react dark:hover:from-react dark:hover:to-react dark:hover:text-white"
           :to="{ name: 'use-with-react', params: { locale } }"
         >
           <React class="self-center p-4" />
           <div class="self-center p-4 pt-0">React</div>
         </RouterLink>
         <RouterLink
-          class="group flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-svelte hover:from-svelte hover:to-svelte hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-svelte dark:hover:from-svelte dark:hover:to-svelte dark:hover:text-white"
+          class="tech-stack-button group h-36 w-40 hover:border-svelte hover:from-svelte hover:to-svelte hover:text-white dark:hover:border-svelte dark:hover:from-svelte dark:hover:to-svelte dark:hover:text-white"
           :to="{ name: 'use-with-svelte', params: { locale } }"
         >
           <Svelte class="self-center p-4" />
           <div class="self-center p-4 pt-0">Svelte</div>
         </RouterLink>
         <RouterLink
-          class="group flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-vue hover:from-vue hover:to-vue hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-vue dark:hover:from-vue dark:hover:to-vue dark:hover:text-white"
+          class="tech-stack-button group h-36 w-40 hover:border-vue hover:from-vue hover:to-vue hover:text-white dark:hover:border-vue dark:hover:from-vue dark:hover:to-vue dark:hover:text-white"
           :to="{ name: 'use-with-vue', params: { locale } }"
         >
           <Vue class="self-center p-4" />
           <div class="self-center p-4 pt-0">Vue 3</div>
         </RouterLink>
         <RouterLink
-          class="flex h-36 w-40 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md hover:border-wordpress hover:from-wordpress hover:to-wordpress hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-wordpress dark:hover:from-wordpress dark:hover:to-wordpress dark:hover:text-white"
+          class="tech-stack-button h-36 w-40 hover:border-wordpress hover:from-wordpress hover:to-wordpress hover:text-white dark:hover:border-wordpress dark:hover:from-wordpress dark:hover:to-wordpress dark:hover:text-white"
           :to="{ name: 'use-with-wordpress', params: { locale } }"
         >
           <WordPress class="self-center p-4" />
@@ -74,10 +74,12 @@ const { t, locale } = useI18n();
         <div
           class="mr-0 mb-6 max-w-xs self-center text-center md:mr-10 md:mb-0 xl:mr-0 xl:mb-10 xl:self-end"
         >
-          If you are using another setup, the default setup is the way to go!
+          If you are using another stack?
+          <br />
+          The default setup is the way to go!
         </div>
         <RouterLink
-          class="flex h-36 w-56 flex-col self-center rounded-lg border-2 border-zinc-400 bg-gradient-to-tr from-zinc-200 to-zinc-100 p-1 text-zinc-700 shadow-md transition hover:border-secondary hover:from-secondary hover:to-secondary hover:text-white hover:no-underline hover:shadow-xl dark:border-zinc-600 dark:from-zinc-800 dark:to-zinc-700 dark:text-zinc-300 dark:hover:border-secondary dark:hover:from-secondary dark:hover:to-secondary dark:hover:text-white"
+          class="tech-stack-button h-36 w-56 hover:border-secondary hover:from-secondary hover:to-secondary hover:text-white dark:hover:border-secondary dark:hover:from-secondary dark:hover:to-secondary dark:hover:text-white"
           :to="{ name: 'use', params: { locale } }"
         >
           <CodeBracketIcon class="self-center p-4" />
--- demo/src/views/GuideView.vue
+++ demo/src/views/GuideView.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts">
 import CodeBlock from "@/components/CodeBlock.vue";
-import { ArrowRightIcon } from '@heroicons/vue/24/outline';
+import { ArrowRightIcon, CodeBracketIcon } from '@heroicons/vue/24/outline';
 import { RouterLink } from "vue-router";
 import { useI18n } from 'vue-i18n'
 const { t, locale } = useI18n();
 </script>
 
 <template>
-  <h1 class="pb-16">The default way to install the Add to Calendar Button</h1>
-  <h2 class="mb-6">Step 1</h2>
-  <p>
-    Load the respective script by adding the following script tag to the
-    <code>&lt;head&gt;</code> section of your website.<br />
-    The script will be loaded in a non-blocking way. So you don't need to worry
-    about where to include it exactly.
-  </p>
-  <CodeBlock>
-    <pre>
+  <div class="flex justify-between">
+    <div class="flex-1 pr-0 lg:pr-8 xl:pr-12 2xl:pr-20">
+      <h1 class="pb-16">How to install the Add to Calendar Button</h1>
+      <div class="px-0 md:px-3 lg:px-5">
+        <h2 class="mb-6">Step 1</h2>
+        <p>
+          Load the respective script by adding the following script tag to the
+          <code>&lt;head&gt;</code> section of your website.<br />
+          The script will be loaded in a non-blocking way. So you don't need to
+          worry about where to include it exactly.
+        </p>
+        <CodeBlock>
+          <pre>
 &lt;script src="https://cdn.jsdelivr.net/npm/add-to-calendar-button@2" async defer&gt;&lt;/script&gt;</pre
-    >
-  </CodeBlock>
-  <h2 class="mt-20 mb-6">Step 2</h2>
-  <p>
-    Start using it by adding a <code>&lt;add-to-calendar-button&gt;</code> tag
-    to your HTML source code and add the options as attributes.
-  </p>
-  <p class="font-semibold italic">Yes, it is that simple.</p>
-  <p>Your code block could look like the following:</p>
-  <CodeBlock class="line-numbers">
-    <pre>
+          >
+        </CodeBlock>
+        <h2 class="mt-20 mb-6">Step 2</h2>
+        <p>
+          Start using it by adding a
+          <code>&lt;add-to-calendar-button&gt;</code> tag to your HTML source
+          code and add the options as attributes.
+        </p>
+        <p class="font-semibold italic">Yes, it is that simple.</p>
+        <p>Your code block could look like the following:</p>
+        <CodeBlock class="line-numbers">
+          <pre>
 &lt;add-to-calendar-button
   name="Title"
   options="'Apple','Google'"
@@ -39,31 +43,45 @@ const { t, locale } = useI18n();
   endTime="23:30"
   timeZone="Europe/Berlin"
 &gt;&lt;/add-to-calendar-button&gt;</pre
+          >
+        </CodeBlock>
+        <p class="mt-20 mb-6">
+          Check out the "Functionality" section for all configuration options.
+        </p>
+        <RouterLink
+          class="button-primary"
+          v-if="locale=='en'"
+          :to="{ name: 'home', hash: '#features', params: { locale } }"
+        >
+          {{ t('labels.startConfiguring') }}
+          <ArrowRightIcon
+            class="-mt-0.5 ml-2 inline-block h-4 w-4"
+            aria-hidden="true"
+          />
+        </RouterLink>
+        <RouterLink
+          class="button-primary"
+          v-if="locale!='en'"
+          :to="{ name: 'home-i18n', hash: '#features', params: { locale } }"
+        >
+          {{ t('labels.startConfiguring') }}
+          <ArrowRightIcon
+            class="-mt-0.5 ml-2 inline-block h-4 w-4"
+            aria-hidden="true"
+          />
+        </RouterLink>
+      </div>
+    </div>
+    <div
+      class="hidden w-48 flex-none border-l border-zinc-300 pl-8 text-xs dark:border-zinc-700 lg:block xl:pl-12"
     >
-  </CodeBlock>
-  <p class="mt-20 mb-6">
-    Check out the "Functionality" section for all configuration options.
-  </p>
-  <RouterLink
-    class="button-primary"
-    v-if="locale=='en'"
-    :to="{ name: 'home', hash: '#features', params: { locale } }"
-  >
-    {{ t('labels.startConfiguring') }}
-    <ArrowRightIcon
-      class="-mt-0.5 ml-2 inline-block h-4 w-4"
-      aria-hidden="true"
-    />
-  </RouterLink>
-  <RouterLink
-    class="button-primary"
-    v-if="locale!='en'"
-    :to="{ name: 'home-i18n', hash: '#features', params: { locale } }"
-  >
-    {{ t('labels.startConfiguring') }}
-    <ArrowRightIcon
-      class="-mt-0.5 ml-2 inline-block h-4 w-4"
-      aria-hidden="true"
-    />
-  </RouterLink>
+      <CodeBracketIcon class="block w-24 pb-28" />
+      <p>You are using Angular, React, Vue, or other frameworks?</p>
+      <p class="font-semibold">Check out the "Installation" section!</p>
+      <p>
+        We have strong guides and solutions for all big frontend tech stacks as
+        well as things like WordPress.
+      </p>
+    </div>
+  </div>
 </template>
