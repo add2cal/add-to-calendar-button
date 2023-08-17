6fe091b84a2afb34dabbbcc84c4c89dcd745e037
Jens Kuerschner <mail@jenskuerschner.de>
2023-08-17 07:43:23 +0200

    fixing test

--- test/server-side-init.test.js
+++ test/server-side-init.test.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 
-// simple import of the atcb JS (base and commonJS version), to ensure it can load in Node
+// simple import of the atcb JS, to ensure it can load in Node
 // it shall not run on the server, but also should not block it!
-require('../dist/atcb.js');
 require('../dist/commonjs/index.js');
