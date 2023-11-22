de19caa3c0dc2d7534096bbebcfa412fc7f1b27d
Jens Kuerschner <mail@jenskuerschner.de>
2023-11-22 21:00:41 +0100

    reverting

--- demo/layouts/default.vue
+++ demo/layouts/default.vue
@@ -27,7 +27,7 @@ const description = computed(() => route.meta.description ? t(route.meta.descrip
           <Meta :id="meta.id" :property="meta.property" :content="meta.content" />
         </template>
         <Meta name="keywords" :content="t('meta.keywords')" />
-        <Meta property="og:title" :content="title" />
+        <Meta property="og:site_name" :content="title" />
         <Meta name="twitter:title" :content="title" />
         <Meta property="og:description" :content="description" />
         <Meta name="twitter:description" :content="description" />
