e2e1421de8cd1bcefed0373ab8630e94e99d2f2d
Jens Kuerschner <mail@jenskuerschner.de>
2023-12-13 17:21:48 +0100

    fixing the fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ function prepareFinalFile(content, stripAllImport = true, stripAllExport = true,
         })();
         // determining the input data. Mind that we remove any multiple "." as well as backslashs for security reasons - should not be used anyway
         const cssFileContent = fs
-          .readFileSync('./assets/css/atcb' + styleString.replace(/[^a-z]/gi, '') + '.min.css')
+          .readFileSync('./assets/css/atcb' + styleString.replace(/[^a-z0-9-]/gi, '') + '.min.css')
           .toString()
           .replace(/\.{2,}/g, '')
           .replace(/\\/g, '')
