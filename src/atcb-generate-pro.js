9f94b43d1d99959dee6e61e04bb12fea404f6f4e
Jens Kuerschner <mail@jenskuerschner.de>
2023-12-19 17:38:45 +0100

    version bump

--- src/atcb-generate-pro.js
+++ src/atcb-generate-pro.js
@@ -3,7 +3,7 @@
  *  Add to Calendar Button
  *  ++++++++++++++++++++++
  *
- *  Version: 2.5.3
+ *  Version: 2.5.4
  *  Creator: Jens Kuerschner (https://jenskuerschner.de)
  *  Project: https://github.com/add2cal/add-to-calendar-button
  *  License: Elastic License 2.0 (ELv2) (https://github.com/add2cal/add-to-calendar-button/blob/main/LICENSE.txt)
