b7ef2a0f35e9f0043529eec0fa755ade06a52c80
Jens Kuerschner <mail@jenskuerschner.de>
2023-12-28 21:43:34 +0100

    better wc types

--- index.d.ts
+++ index.d.ts
@@ -44,7 +44,7 @@ declare module 'add-to-calendar-button/no-pro-unstyle' {
 }
 
 // INTERFACES
-interface ATCBActionEventConfig {
+export interface ATCBActionEventConfig {
   proKey?: string;
   name?: string;
   dates?: EventDate[];
@@ -107,7 +107,84 @@ interface ATCBActionEventConfig {
   dev?: boolean;
 }
 
-interface EventDate {
+export type AddToCalendarButtonType = {
+  proKey?: string;
+  name?: string;
+  dates?: EventDate[];
+  description?: string;
+  startDate?: string;
+  startTime?: string;
+  endDate?: string;
+  endTime?: string;
+  timeZone?: string;
+  location?: string;
+  status?: 'TENTATIVE' | 'CONFIRMED' | 'CANCELLED';
+  sequence?: number;
+  uid?: string;
+  organizer?: string;
+  attendee?: string;
+  icsFile?: string;
+  images?: string[] | string;
+  recurrence?: string;
+  recurrence_interval?: number;
+  recurrence_until?: string;
+  recurrence_count?: number;
+  recurrence_byDay?: string;
+  recurrence_byMonth?: string;
+  recurrence_byMonthDay?: string;
+  recurrence_weekstart?: string;
+  availability?: 'busy' | 'free';
+  created?: string;
+  updated?: string;
+  identifier?: string;
+  subscribe?: boolean;
+  options?: ('Apple' | 'Google' | 'iCal' | 'Microsoft365' | 'MicrosoftTeams' | 'Outlook.com' | 'Yahoo')[];
+  optionsMobile?: ('Apple' | 'Google' | 'iCal' | 'Microsoft365' | 'MicrosoftTeams' | 'Outlook.com' | 'Yahoo')[];
+  optionsIOS?: ('Apple' | 'Google' | 'iCal' | 'Microsoft365' | 'MicrosoftTeams' | 'Outlook.com' | 'Yahoo')[];
+  iCalFileName?: string;
+  listStyle?: 'dropdown' | 'dropdown-static' | 'dropup-static' | 'overlay' | 'modal';
+  buttonStyle?: 'default' | '3d' | 'flat' | 'round' | 'neumorphism' | 'text' | 'date' | 'custom' | 'none';
+  trigger?: 'hover' | 'click';
+  inline?: boolean;
+  buttonsList?: boolean;
+  hideIconButton?: boolean;
+  hideIconList?: boolean;
+  hideIconModal?: boolean;
+  hideTextLabelButton?: boolean;
+  hideTextLabelList?: boolean;
+  hideBackground?: boolean;
+  hideCheckmark?: boolean;
+  hideBranding?: boolean;
+  hideButton?: boolean;
+  size?: string;
+  label?: string;
+  inlineRsvp?: string;
+  customLabels?: CustomLabelsObjectType;
+  customCss?: string;
+  lightMode?: 'system' | 'dark' | 'light' | 'bodyScheme';
+  language?: 'en' | 'de' | 'nl' | 'fa' | 'fr' | 'es' | 'et' | 'pt' | 'tr' | 'zh' | 'ar' | 'hi' | 'pl' | 'ro' | 'id' | 'no' | 'fi' | 'sv' | 'cs' | 'ja' | 'it' | 'ko' | 'vi';
+  hideRichData?: boolean;
+  ty?: object;
+  rsvp?: object;
+  bypassWebViewCheck?: boolean;
+  debug?: boolean;
+  cspnonce?: string;
+  blockInteraction?: boolean;
+  styleLight?: string;
+  styleDark?: string;
+  disabled?: boolean;
+  hidden?: boolean;
+  pastDateHandling?: string;
+  proxy?: boolean;
+  fakeMobile?: boolean;
+  fakeIOS?: boolean;
+  fakeAndroid?: boolean;
+  forceOverlay?: boolean;
+  instance?: number;
+  dev?: boolean;
+};
+
+export interface EventDate {
   name?: string;
   description?: string;
   startDate?: string;
@@ -126,3 +203,14 @@ interface EventDate {
 export type CustomLabelsObjectType = {
   [key: string]: string | null;
 };
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'add-to-calendar-button': AddToCalendarButtonType;
+  }
+  namespace JSX {
+    interface IntrinsicElements {
+      ['add-to-calendar-button']: AddToCalendarButtonType;
+    }
+  }
+}
