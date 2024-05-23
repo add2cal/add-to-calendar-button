fef0d9d59945060c220a7e264ec78f3616698429
Jens Kuerschner <mail@jenskuerschner.de>
2024-05-23 09:13:19 +0200

    fixes

--- demo/components/header.vue
+++ demo/components/header.vue
@@ -10,7 +10,9 @@ const localePath = useLocalePath();
   <div :class="view.atTopOfPage ? 'py-5 shadow-lg' : 'py-3 shadow-xl lg:py-5 lg:shadow-lg'" class="fixed top-0 z-50 w-full bg-gradient-to-tr from-primary from-50% to-primary-light transition-all dark:from-primary-darkest dark:to-primary-darker lg:relative">
     <div class="container flex justify-between">
       <div :class="view.atTopOfPage ? '-ml-2 max-w-[150px]' : '-ml-3 max-w-[100px] lg:-ml-2 lg:max-w-[150px]'" class="-mt-1 w-auto grow transition-all xl:ml-0">
-        <NuxtLink :to="localePath('index')"><Logo class="force-light" /></NuxtLink>
+        <NuxtLink :to="localePath('index')">
+          <Logo class="force-light" />
+        </NuxtLink>
       </div>
       <NavigationBar />
     </div>
