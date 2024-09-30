89bde6cb5a3f4ef54934436465d3bbf864601267
Jens Kuerschner <mail@jenskuerschner.de>
2024-09-30 22:45:13 +0200

    attribution fix

--- test/wc-tests/wc-load.test.js
+++ test/wc-tests/wc-load.test.js
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-undef */
-
 import { fixture, html, expect } from '@open-wc/testing';
 import '../../dist/module/index.js';
 
