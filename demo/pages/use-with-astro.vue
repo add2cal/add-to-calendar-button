95b7436a13d0fb50f48c87f5abb07eb7a6c8da2e
Jens Kuerschner <code@jekuer.com>
2026-07-15 21:36:10 +0200

    astro docs fix

--- demo/pages/use-with-astro.vue
+++ demo/pages/use-with-astro.vue
@@ -1,29 +1,10 @@
 <script setup lang="ts">
 import GuideSidebar from "@/components/integration/guideSidebar.vue";
 const LazyCodeBlock = defineAsyncComponent(() => import('@/components/codeBlock.vue'));
-const { locale } = useI18n();
 
 definePageMeta({
   title: 'navigation.use-with-astro',
   description: 'meta.use-with-astro.description',
-});
-
-const today = new Date();
-const nextDay = new Date();
-nextDay.setDate(today.getDate() + 3);
-const defaultDate = nextDay.getFullYear() + '-' + ('0' + (nextDay.getMonth() + 1)).slice(-2) + '-' + ('0' + nextDay.getDate()).slice(-2);
-let defaultLang = (function () {
-  if (locale.value != 'en') {
-    return '\n      language="' + locale.value + '"';
-  }
-  return '';
-})();
-watch(locale, value => {
-  if (value != 'en') {
-    defaultLang = '\n      language="' + locale.value + '"';
-  } else {
-    defaultLang = '';
-  }
 });
 </script>
 
@@ -35,51 +16,46 @@ watch(locale, value => {
         <h2 class="mb-6">{{ $t('content.guide.step0') }}: {{ $t('content.guide.astro.step_pick_solution') }}</h2>
         <p>{{ $t('content.guide.astro.intro_1') }}</p>
         <p>{{ $t('content.guide.astro.intro_2') }}</p>
-        <h2 class="mb-6 mt-20">{{ $t('content.guide.step1') }}: {{ $t('content.guide.astro.step_setup') }}</h2>
-        <h3 class="mb-6">{{ $t('content.guide.astro.setup_1') }}</h3>
+        <h2 class="mb-6 mt-20">{{ $t('content.guide.step1') }}: {{ $t('content.guide.astro.npm_setup_1') }}</h2>
+        <p>{{ $t('content.guide.astro.npm_setup_2') }}</p>
+        <LazyCodeBlock language="shell"><pre>npm install add-to-calendar-button</pre></LazyCodeBlock>
+        <h2 class="mb-6 mt-20">{{ $t('content.guide.step2') }}: {{ $t('content.guide.astro.setup_1') }}</h2>
         <p>{{ $t('content.guide.astro.new_component') }}</p>
-        <h3 class="mb-6 mt-20">{{ $t('content.guide.astro.setup_2') }}</h3>
-        <p>{{ $t('content.guide.astro.npm_setup_1') }}</p>
-        <LazyCodeBlock language="shell"><pre>npm install add-to-calendar-button-react</pre></LazyCodeBlock>
-        <div class="mb-6 mt-10 font-bold">{{ $t('content.guide.astro.npm_setup_2') }}</div>
-        <LazyCodeBlock language="js"><pre>import { AddToCalendarButton } from 'add-to-calendar-button-react';</pre></LazyCodeBlock>
-        <h2 class="mb-6 mt-20">{{ $t('content.guide.step2') }}: {{ $t('content.guide.astro.step_define') }}</h2>
-        <p>
-          {{ $t('content.guide.astro.define') }}
-        </p>
+        <h2 class="mb-6 mt-20">{{ $t('content.guide.step3') }}: {{ $t('content.guide.astro.step_define') }}</h2>
+        <p>{{ $t('content.guide.astro.define') }}</p>
         <LazyCodeBlock language="js">
           <pre>
-import { AddToCalendarButton } from 'add-to-calendar-button-react';
-import type { AddToCalendarButtonType } from 'add-to-calendar-button-react';
+import type { AddToCalendarButtonType } from 'add-to-calendar-button';
 
-export default function atcb(props: AddToCalendarButtonType) {
-  return (
-    &lt;AddToCalendarButton
-      label={props.label}
-      name="{{ $t('demo_data.name_dummy') }}"
-      options="'Apple','Google'"
-      location="{{ $t('demo_data.location') }}"
-      startDate="{{ defaultDate }}"
-      endDate="{{ defaultDate }}"
-      startTime="10:15"
-      endTime="23:30"
-      timeZone="{{ $t('demo_data.default_timezone') }}"{{ defaultLang }}
-      options="'Apple','Google','iCal','Outlook.com','Yahoo'"
-    &gt;&lt;/AddToCalendarButton&gt;
-  );
-}</pre>
+interface Props {
+  prokey: AddToCalendarButtonType['proKey'];
+}
+
+const { prokey } = Astro.props as Props;</pre>
         </LazyCodeBlock>
-        <h2 class="mb-6 mt-20">{{ $t('content.guide.step3') }}: {{ $t('content.guide.step_use') }}</h2>
+        <h2 class="mb-6 mt-20">{{ $t('content.guide.astro.setup_2') }}</h2>
+        <p>{{ $t('content.guide.astro.add_block') }}</p>
+        <LazyCodeBlock language="html">
+          <pre>&lt;add-to-calendar-button prokey={prokey} /&gt;</pre>
+        </LazyCodeBlock>
+        <p>{{ $t('content.guide.astro.add_script') }}</p>
+        <LazyCodeBlock language="html">
+          <pre>
+&lt;script&gt;
+  import 'add-to-calendar-button';
+&lt;/script&gt;</pre>
+        </LazyCodeBlock>
+        <h2 class="mb-6 mt-20">{{ $t('content.guide.step4') }}: {{ $t('content.guide.step_use') }}</h2>
         <p>{{ $t('content.guide.astro.use') }}</p>
         <p>{{ $t('content.guide.step_use_example') }}</p>
-        <LazyCodeBlock>
+        <LazyCodeBlock language="html">
           <pre>
 ---
-import AddToCalendarButton from '../components/AddToCalendarButton.tsx';
+import AddToCalendarButton from '../components/add-to-calendar.astro';
 import Layout from '../layouts/Layout.astro';
 ---
 &lt;Layout&gt;
-&lt;AddToCalendarButton client:only="react" label="Woohooo" /&gt;
+  &lt;AddToCalendarButton prokey="prokey-of-your-event" /&gt;
 &lt;/Layout&gt;</pre>
         </LazyCodeBlock>
         <p class="font-semibold italic">{{ $t('content.guide.step_use_simple') }}</p>
